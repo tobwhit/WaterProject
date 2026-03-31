90c2dbb60239b15a6d24f6ea3617089a1fdfdf3f
tobwhit <tobin.whitworth@gmail.com>
2026-03-31 16:30:41 -0600

    Cors changes

--- backend/WaterProject.api/Program.cs
+++ backend/WaterProject.api/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<WaterDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("WaterDbContext")));
 builder.Services.AddCors(options => options.AddPolicy( "AllowReactApp", policy =>
 {
-    policy.WithOrigins("http://localhost:3003")
+    policy.AllowAnyOrigin()
         .AllowAnyMethod()
         .AllowAnyHeader();
 }));
--- backend/WaterProject.api/obj/Debug/net8.0/WaterProject.API.AssemblyInfo.cs
+++ backend/WaterProject.api/obj/Debug/net8.0/WaterProject.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42bcabd40f3356e7c7e3d4d483ae170473211a68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eefb25d34c1a7a215a435324480f636aec77cf66")]
 [assembly: System.Reflection.AssemblyProductAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
